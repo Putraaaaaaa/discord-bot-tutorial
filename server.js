70be225eb0f4c862d0df44bb114665f4bab1f69e
RanggaGultom <41747668+RanggaGultom@users.noreply.github.com>
2020-07-09 18:39:09 +0700

    part #2 by rayhantekh

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-
-client.on('ready', async () => {
-  console.log(`${client.user.tag} sudah online!`)
-  client.user.setActivity("Jangan Lupa Subscribe")//bisa diganti sesuai keinginan
+const Discord = require("discord.js");
+const client = new Discord.Client();
+//UPTIME ROBOT (WEB)
+const { get } = require("snekfetch");
+const http = require("http");
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  console.log("Pinging");
+  response.sendStatus(200);
 })
+app.listen(process.env.PORT);
+setInterval(() => {
 
-client.on('message', async message => {
-  if(message.content === "indonesia") message.reply(":flag_id:")
-})
-client.login("token bot kamu")
+http.get('live app http');
+}, 280000);
+
+client.on("ready", async () => {
+  console.log(`${client.user.tag} sudah online!`);
+  client.user.setActivity("Jangan Lupa Subscribe");
+});
+
+client.on("message", async message => {
+  if (message.content === "indonesia") message.reply(":flag_id:");
+});
+client.login("bot token");
